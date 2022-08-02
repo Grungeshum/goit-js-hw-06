28af38ac1cc0a8c6409a529978c8ed204628e12f
Igor Mazur <62945459+Grungeshum@users.noreply.github.com>
2022-08-02 09:24:23 +0300

    adds task#2

--- js/task-02.js
+++ js/task-02.js
@@ -5,4 +5,11 @@ const ingredients = [
   'Tomatos',
   'Herbs',
   'Condiments',
-];
+]
+const listEl = document.querySelector('#ingredients')
+console.log(listEl)
+
+const itemEl = ingredients.reduce((str, item) => str + `<li>${item}</li>`, '')
+console.log(itemEl)
+
+listEl.innerHTML = itemEl
